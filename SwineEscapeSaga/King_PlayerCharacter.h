a2a8e9e2962006de1df96c780b1af2e4e6effdc6
Mouayad <84500993+Mouayad91@users.noreply.github.com>
2024-08-02 14:09:23 +0200

    first commit

---
+++ SwineEscapeSaga/King_PlayerCharacter.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PaperZDCharacter.h"
+#include "King_PlayerCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SWINEESCAPESAGA_API AKing_PlayerCharacter : public APaperZDCharacter
+{
+	GENERATED_BODY()
+	
+};
---
+++ SwineEscapeSaga/SwineEscapeSagaGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SwineEscapeSagaGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SWINEESCAPESAGA_API ASwineEscapeSagaGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
