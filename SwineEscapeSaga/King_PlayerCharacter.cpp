84c3ae884dba55f8f4e3efb70e8cfbb4ecd40b8f
Mouayad <84500993+Mouayad91@users.noreply.github.com>
2024-08-02 15:19:32 +0200

    Component setup

--- SwineEscapeSaga/King_PlayerCharacter.cpp
+++ SwineEscapeSaga/King_PlayerCharacter.cpp
@@ -1,5 +1,26 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
+// Fill out your copyright notice in the Description page of Project Settings.
 
 #include "King_PlayerCharacter.h"
 
+AKing_PlayerCharacter::AKing_PlayerCharacter()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	SpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArm"));
+	SpringArm->SetupAttachment(RootComponent);
+
+	PlayerCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
+	PlayerCamera->SetupAttachment(SpringArm, USpringArmComponent::SocketName);
+}
+
+void AKing_PlayerCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void AKing_PlayerCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
--- SwineEscapeSaga/King_PlayerCharacter.h
+++ SwineEscapeSaga/King_PlayerCharacter.h
@@ -4,14 +4,27 @@
 
 #include "CoreMinimal.h"
 #include "PaperZDCharacter.h"
+#include "Camera/CameraComponent.h"
+#include "GameFramework/SpringArmComponent.h"
 #include "King_PlayerCharacter.generated.h"
 
 /**
- * 
+ *
  */
 UCLASS()
 class SWINEESCAPESAGA_API AKing_PlayerCharacter : public APaperZDCharacter
 {
 	GENERATED_BODY()
-	
+
+public:
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
+	USpringArmComponent* SpringArm;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
+	UCameraComponent* PlayerCamera;
+
+	AKing_PlayerCharacter();
+	virtual void BeginPlay() override;
+	virtual void Tick(float DeltaTime) override;
 };
